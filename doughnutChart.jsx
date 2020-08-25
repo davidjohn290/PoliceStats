aaa84509dbe8323e4986eaf12f06b3f9cd1fec12
David John <davidspc@MacBook-Air.local>
2020-08-25 16:12:33 +0100

    data chart complete

--- doughnutChart.jsx
+++ doughnutChart.jsx
@@ -1,13 +1,61 @@
-const data = {
-  labels: ["Red", "Green", "Yellow"],
+import React, { Component } from "react";
+import { Doughnut } from "react-chartjs-2";
+const { getProperties, putDataIntoObject } = require("../utilis/getProperties");
 
-  datasets: [
-    {
-      data: [300, 50, 100],
-      backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-      hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-    },
-  ],
-};
+class DoughnutChart extends Component {
+  state = {
+    city: "cheshire",
+    policeDataKeys: [],
+    policeDataValues: [],
+  };
 
-export default data;
+  componentDidMount() {
+    console.log("has mounted doughnut");
+    this.getPoliceChartData(this.state.city).then((res) => {
+      const keys = Object.keys(res);
+      const values = Object.values(res);
+      this.setState({ policeDataKeys: keys, policeDataValues: values });
+    });
+  }
+
+  getPoliceChartData(city) {
+    return fetch(`https://data.police.uk/api/stops-force?force=${city}`)
+      .then((data) => {
+        return data.json();
+      })
+      .then((res) => {
+        const result = getProperties(res, "object_of_search");
+        const dataObject = putDataIntoObject(result);
+        return dataObject;
+      });
+  }
+
+  handleConsoleLog = (clickEvent) => {
+    clickEvent.preventDefault();
+    console.log(this.props);
+  };
+
+  render() {
+    const data = {
+      labels: this.state.policeDataKeys,
+      datasets: [
+        {
+          data: this.state.policeDataValues,
+          backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+          hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
+        },
+      ],
+    };
+    return (
+      <div>
+        <div className="chart">
+          <p>Graph of reasons for stop and searches</p>
+          <Doughnut data={data} />
+        </div>
+        <button onClick={this.handleConsoleLog}>Console Log</button>
+      </div>
+    );
+  }
+}
+
+export default DoughnutChart;
